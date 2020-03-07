912be8da2986b3a44f58357aae8b3f77197afe8e
Nguyen Van Giang <44034128+gnvml@users.noreply.github.com>
2020-03-07 17:53:10 +0700

    hide terminal

--- shortcut.py
+++ shortcut.py
@@ -4,6 +4,6 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 path_file = os.path.join(base_dir, 'tdstatv3.py')
 path_icon = os.path.join(base_dir, 'icon.ico')
 make_shortcut(path_file, name ='OEPS',
-                        icon = path_icon)
+                        icon = path_icon, terminal = False)
 
 
